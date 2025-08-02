034adf279626c5e2c9138530c88f8a0db7f9cb2d
saran2002ns <sarannadarajan2002@gmail.com>
2025-08-02 15:46:04 +0530

    v1.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "../dist/output.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
